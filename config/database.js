9e1ddb3790de04dd435eaf8a1c1ac2484addde38
Yusuf Adedeji <adedejiyusuf26@gmail.com>
2025-05-29 03:18:04 +0100

    made changes

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
   // database: "mongodb://localhost:27017/prof",
   database:
-    "mongodb+srv://aboaba:aboaba123@prof-aboaba.khlnx.mongodb.net/prof?retryWrites=true&w=majority",
+    "mongodb+srv://aboaba:aboaba123@cluster0.9vys2qh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
 };
